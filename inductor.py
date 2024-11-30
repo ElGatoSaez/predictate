0c2ac9bdd88061b6ead8847bb5197ff31b030265
ElGatoSaez <10396037+ElGatoSaez@users.noreply.github.com>
2024-11-30 00:56:43 -0300

    unowoed

--- inductor.py
+++ inductor.py
@@ -2,12 +2,26 @@ import json
 import mysql.connector
 import argparse
 
-# Conexión a la base de datos
+from dotenv import load_dotenv
+import os
+
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
+
+# Acceder a las credenciales
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+# Conectar a la base de datos
+import mysql.connector
+
 db = mysql.connector.connect(
-    host="localhost",
-    user="predictate",
-    password="miaucito",
-    database="predictate"
+    host=DB_HOST,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    database=DB_NAME
 )
 
 cursor = db.cursor()
